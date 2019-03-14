b23504465974a6a7e72b7ff84093af1e04cea299
Hk4Fun <941222165nobody@gmail.com>
2019-03-14 22:13:29 +0800

    PEP8

--- back_end/app/__init__.py
+++ back_end/app/__init__.py
@@ -11,8 +11,7 @@ parser_listen.add_argument('-p', dest='port', required=True, type=int, help='Por
 parser_listen.add_argument('-a', dest='addr', default='0.0.0.0:8000',
                            help='Address for a web server to manage users, default 0.0.0.0:8000')
 parser_listen.add_argument('-v', '--verbose', action='count', dest='level',
-                    default=2, help='verbose log (repeat for more verbose)')
-
+                           default=2, help='verbose log (repeat for more verbose)')
 
 parser_slave = subparsers.add_parser('slave', help='run in slave mode')
 parser_slave.add_argument('-b', dest='bind', type=int, default=0,
@@ -22,7 +21,7 @@ parser_slave.add_argument('-l', dest='local', required=True, help='Local server 
 parser_slave.add_argument('-r', dest='remote', required=True, help='Remote listen address in format host:port')
 parser_slave.add_argument('-u', dest='user', required=True, help='User in format username:password')
 parser_slave.add_argument('-v', '--verbose', action='count', dest='level',
-                    default=2, help='verbose log (repeat for more verbose)')
+                          default=2, help='verbose log (repeat for more verbose)')
 
 args = parser.parse_args()
 
